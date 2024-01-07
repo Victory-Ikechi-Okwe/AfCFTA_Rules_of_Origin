ec959b52a15aceccf8ac849249057beb3db5c12d
Don Kelly <karfai@gmail.com>
2024-01-07 13:09:53 -0500

    [wip] implements in_effect insertion and db schema screation

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,6 @@
 use log::*;
 use rusqlite::{Connection};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct InEffect {
@@ -13,17 +14,38 @@ pub fn store(id: String, effects: &Vec<InEffect>) -> bool {
 //    let conn = Connection::open("data/rules.db")?;
     let conn = Connection::open("data/rules.db").unwrap();
 
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS in_effect (
-              id           INTEGER PRIMARY KEY AUTOINCREMENT,
-              rule_id      text,
-              version      text,
-              jurisdiction text,
-              from_t       text,
-              to_t         text,
-              tz           text
-         )", []).unwrap();
+    conn.execute_batch(
+        "BEGIN;
+         CREATE TABLE IF NOT EXISTS in_effect (
+               id           INTEGER PRIMARY KEY AUTOINCREMENT,
+               rule_id      text,
+               version      text,
+               jurisdiction text,
+               from_t       text,
+               to_t         text,
+               tz           text
+         );
+         CREATE TABLE IF NOT EXISTS applicable (
+               id      int  PRIMARY KEY,
+               rule_id text,
+               version text,
+               key     text
+         );
+         COMMIT;").unwrap();
+
+    let mut stmt = conn.prepare("
+      INSERT INTO in_effect (rule_id, version, jurisdiction, from_t, to_t, tz)
+                     VALUES (?1, ?2, ?3, ?4, ?5, ?6)").unwrap();
     for ie in effects.iter() {
+        stmt.execute([
+            id.clone(),
+            String::from(""),
+            ie.loc.clone(),
+            ie.from.clone(),
+            ie.to.clone(),
+            ie.tz.clone(),
+        ]).unwrap();
+
         debug!("store: {:?} = {:?}", id, ie);
     }
     true
