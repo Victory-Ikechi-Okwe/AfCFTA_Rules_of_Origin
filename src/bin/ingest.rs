78b1733ce4b3a4b378cead072dd9d13900165ba5
Don Kelly <karfai@gmail.com>
2024-09-22 10:34:52 -0400

    Properly record revisions and store revised file

--- src/bin/ingest.rs
+++ src/bin/ingest.rs
@@ -1,11 +1,17 @@
 use log::*;
 use rusqlite::Connection;
-use std::env;
+use glob::glob;
+use std::{
+    env,
+    path::PathBuf,
+};
 
 fn open_db() -> Connection {
     let should_init = !std::path::Path::new("data/rules.db").exists();
 
-    std::fs::create_dir("./data").expect("failed to create directory");
+    if !std::path::Path::new("data").exists() {
+        std::fs::create_dir("./data").expect("failed to create directory");
+    }
 
     let conn = Connection::open("data/rules.db").unwrap();
 
@@ -41,14 +47,14 @@ struct InEffect {
     pub tz: String,
 }
 
-fn store_in_effect(conn: &Connection, id: &String, in_effect: &Vec<InEffect>) {
+fn store_in_effect(conn: &Connection, id: &String, rev: u64, in_effect: &Vec<InEffect>) {
     let mut stmt = conn.prepare("
       INSERT INTO in_effect (rule_id, version, jurisdiction, from_t, to_t, tz)
                      VALUES (?1, ?2, ?3, ?4, ?5, ?6)").unwrap();
     for ie in in_effect.iter() {
         stmt.execute([
             id.clone(),
-            String::from(""),
+            rev.to_string(),
             ie.loc.clone(),
             ie.from.clone(),
             ie.to.clone(),
@@ -68,12 +74,39 @@ fn make_in_effect(o: &serde_json::Value) -> InEffect {
     }
 }
 
-pub fn store_keys(conn: &Connection, id: &String, keys: &Vec<String>) -> bool {
+pub fn store_keys(conn: &Connection, id: &String, rev: u64, keys: &Vec<String>) -> bool {
     let mut stmt = conn.prepare("INSERT INTO applicable (rule_id, version, key) VALUES (?1, ?2, ?3)").unwrap();
     for k in keys.iter() {
-        stmt.execute([id.clone(), String::from(""), k.clone()]).unwrap();
+        stmt.execute([id.clone(), rev.to_string(), k.clone()]).unwrap();
     }
     true
+}
+
+fn rule_dir(id: &String) -> PathBuf {
+    [".", "data", "rules", id].iter().collect()
+}
+
+fn extract_rev(p: &PathBuf) -> u64 {
+    match p.as_path().file_stem() {
+        None => 0,
+        Some(st) => { st.to_str().unwrap().parse().unwrap() }
+    }
+}
+
+fn find_latest_rule(id: &String) -> Option<u64> {
+    let dir = rule_dir(id);
+    let vers = dir.join("*.json");
+
+    println!("searching for rules: vers={:?}", vers);
+    let latest = match glob(vers.to_str().unwrap()) {
+        Ok(it) => it.filter_map(|p| p.ok()).max_by_key(extract_rev),
+        _ => None
+    };
+
+    match latest {
+        Some(p) => Some(extract_rev(&p)),
+        None => None
+    }
 }
 
 // follows an update-or-insert model: if the rule has an 'id' property,
@@ -98,12 +131,34 @@ fn main() {
             let conn = open_db();
             println!("id={:?}", id);
 
-            // TODO: retain rule and add version to DB
+            let rev = match find_latest_rule(&id) {
+                Some(r) => r + 1,
+                None => 0
+            };
+            let path = rule_dir(&id).join(format!("{:?}.json", rev));
+            println!("rev={:?}; path={:?}", rev, path);
+
+            // TODO: design a binary format to retain, not JSON, which we need to parse
+            match std::fs::File::create(&path) {
+                Ok(f) => {
+                    match serde_json::to_writer(f, &o) {
+                        Ok(_) => {
+                            println!("wrote rule (rule={:?}", path);
+                        },
+                        Err(e) => {
+                            println!("failed to write rule (rule={:?}; e={:?})", path, e);
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("failed to create file (rule={:?}; e={:?}", path, e);
+                }
+            }
 
             match &o["in_effect"] {
                 serde_json::Value::Array(ie) => {
                     let vals: Vec<InEffect> = ie.iter().map(make_in_effect).collect();
-                    store_in_effect(&conn, &id, &vals);
+                    store_in_effect(&conn, &id, rev, &vals);
                 },
                 _ => {
                     println!("no in effect");
@@ -120,7 +175,7 @@ fn main() {
                     }).flatten().collect();
 
                     debug!("keys: {:?}", keys);
-                    store_keys(&conn, &id, &keys);
+                    store_keys(&conn, &id, rev, &keys);
                 },
                 _ => {
                     debug!("no conditions");
