6de4650eedfec764f839ce8738ba39cdaf0a9ea7
Don Kelly <karfai@gmail.com>
2024-09-28 07:37:39 -0400

    Completes JSON parsing for invoke

--- src/bin/invoke.rs
+++ src/bin/invoke.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{
+    env,
+    path::PathBuf,
+};
 
 // given: doc.json rule in processable form
 // output: oughts from rule
@@ -9,8 +12,38 @@ fn watch() {
     println!("watching");
 }
 
+fn rule_dir(id: &String) -> PathBuf {
+    [".", "data", "rules", id].iter().collect()
+}
+
+fn parse_json_file(path: &PathBuf) -> Option<serde_json::Value> {
+    let f = std::fs::File::open(path).expect("could not open file");
+    match serde_json::from_reader(f) {
+        Result::Ok(o) => Some(o),
+        Err(_) => None,
+    }
+}
+
 fn single_run(path: &String, id: &String, rev: u64) {
     println!("single run: path={:?}; id={:?}; rev={:?}", path, id, rev);
+
+    let doc_path = PathBuf::from(path);
+    let rule_path = rule_dir(&id).join(format!("{:?}.json", rev));
+
+    match [parse_json_file(&doc_path), parse_json_file(&rule_path)] {
+        [Some(doc), Some(rule)] => {
+            println!("have both: {:?}, {:?}", doc, rule);
+        },
+        [Some(_), None] => {
+            println!("no rule");
+        },
+        [None, Some(_)] => {
+            println!("no doc");
+        },
+        [None, None] => {
+            println!("neither");
+        }
+    }
 }
 
 fn main() {
