c2f6e823ad8a0b8ddffd0fdcca351160220e5161
Don Kelly <karfai@gmail.com>
2024-09-28 07:18:17 -0400

    Starts argument processing for invoke

--- src/bin/invoke.rs
+++ src/bin/invoke.rs
@@ -1,10 +1,28 @@
 use std::env;
 
+// given: doc.json rule in processable form
+// output: oughts from rule
+//
+// processable form:
+// - all JSON leaves exploded into a k,v table
+fn watch() {
+    println!("watching");
+}
+
+fn single_run(path: &String, id: &String, rev: u64) {
+    println!("single run: path={:?}; id={:?}; rev={:?}", path, id, rev);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        dbg!("watch");
-    } else {
-        dbg!(&args[1]);
+    match args.as_slice() {
+        [_] => watch(),
+        [_, path, id, rev_s] => {
+            let rev = rev_s.parse::<u64>().expect("failed to parse rev");
+            single_run(&path, &id, rev);
+        }
+        _ => {
+            println!("invalid args");
+        }
     }
 }
