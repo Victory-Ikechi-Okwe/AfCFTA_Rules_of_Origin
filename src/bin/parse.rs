cda9ebb141da17f1a30bc7000e463f5d0fcda289
DK <karfai@gmail.com>
2025-05-17 08:37:16 -0400

    Changes to clap for arg parsing in bin/parse

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -3,17 +3,19 @@ use std::io::{ self };
 
 use rookie::rules::parser;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    rules_fn: String,
+}
+
 fn main() -> io::Result<()> {
-    let fln = match env::args().nth(1) {
-        Some(a) => a,
-        None => {
-            eprintln!("give me a file name");
-            std::process::exit(1);
-        }
-    };
+    let args = Args::parse();
 
     let mut prsr = parser::Parse::new();
-    prsr.parse_file(&fln)?;
+    prsr.parse_file(&args.rules_fn)?;
     println!("{:?}", prsr.rule);
 
     Ok(())
